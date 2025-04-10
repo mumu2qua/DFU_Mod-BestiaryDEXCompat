eea180de95139ba91be7c35f3c09df6b03c84491
Mumu2qua <hriktux@keemail.me>
2025-04-10 09:23:18 +0700

    Clean up

--- Scripts/BestiaryDEXCompat.cs
+++ Scripts/BestiaryDEXCompat.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using DaggerfallConnect;
 using DaggerfallWorkshop.Game;
@@ -13,9 +14,9 @@ namespace BestiaryDEXCompat
     internal class TextFields
     {
         internal string MONTITLE  { get; private set; }
-        internal string MONTITLE2 { get; private set; }
+        internal string MMONTITLE { get; private set; }
         internal string MONSUM    { get; private set; }
-        internal string MONSUM2   { get; private set; }
+        internal string MMONSUM   { get; private set; }
         internal string ANTITLE   { get; private set; }
         internal string ANSUM     { get; private set; }
         internal string UNDTITLE  { get; private set; }
@@ -28,9 +29,9 @@ namespace BestiaryDEXCompat
         internal TextFields(Mod mod)
         {
             MONTITLE  = mod.Localize("MONTITLE");
-            MONTITLE2 = mod.Localize("MONTITLE2");
+            MMONTITLE = mod.Localize("MMONTITLE");
             MONSUM    = mod.Localize("MONSUM");
-            MONSUM2   = mod.Localize("MONSUM2");
+            MMONSUM   = mod.Localize("MMONSUM");
             ANSUM     = mod.Localize("ANSUM");
             ANTITLE   = mod.Localize("ANTITLE");
             UNDSUM    = mod.Localize("UNDSUM");
@@ -39,6 +40,20 @@ namespace BestiaryDEXCompat
             DAETITLE  = mod.Localize("DAETITLE");
             GOLSUM    = mod.Localize("GOLSUM");
             GOLTITLE  = mod.Localize("GOLTITLE");
+        }
+    }
+
+    internal class BestiaryGroup
+    {
+        public string[] Entities;
+        public string Title;
+        public string Summary;
+
+        public BestiaryGroup(string[] entities, string title, string summary)
+        {
+            Entities = entities;
+            Title = title;
+            Summary = summary;
         }
     }
 
@@ -58,32 +73,25 @@ namespace BestiaryDEXCompat
         void Awake()
         {
             TextFields = new TextFields(mod);
-            // Animal page
-            // Looks kinda dumb, but I tried registering all the entities at once but it didn't work until I split it
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "Wolf", "SnowWolf", "BloodSpider", "MountainLion", "Boar", "Mudcrab", "Dog" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.ANTITLE, TextFields.ANSUM, new string[] { "Wolf",
-            "SnowWolf", "BloodSpider", "MountainLion", "Boar", "Mudcrab", "Dog" } });
 
-            // Monster pages
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "Goblin", "Homunculus", "Lizardman", "LizardmanWarrior", "Grotesque",
-            "Minotaur", "Medusa", "Troll", "Ogre" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.MONTITLE, TextFields.MONSUM, new string[] { "Goblin", "Homunculus", "Lizardman", "LizardmanWarrior", "Grotesque", "Minotaur", "Medusa", "Troll", "Ogre" } });
+            var bestiaryGroups = new BestiaryGroup[]
+            {
+                new BestiaryGroup(new[] { "Wolf", "SnowWolf", "BloodSpider", "MountainLion", "Boar", "Mudcrab", "Dog" }, TextFields.ANTITLE, TextFields.ANSUM),
+                new BestiaryGroup(new[] { "Goblin", "Homunculus", "Lizardman", "LizardmanWarrior", "Grotesque", "Minotaur", "Medusa", "Troll", "Ogre" }, TextFields.MONTITLE, TextFields.MONSUM),
+                new BestiaryGroup(new[] { "LandDreugh", "Wisp" }, TextFields.MMONTITLE, TextFields.MMONSUM),
+                new BestiaryGroup(new[] { "SkeletalSoldier", "GloomWraith", "FadedGhost", "Ghoul", "DireGhoul" }, TextFields.UNDTITLE, TextFields.UNDSUM),
+                new BestiaryGroup(new[] { "HellHound", "FireDaemon", "Dremora", "Scamp" }, TextFields.DAETITLE, TextFields.DAESUM),
+                new BestiaryGroup(new[] { "IronGolem", "IceGolem", "StoneGolem", "DwarvenSphere", "DwarvenSteam" }, TextFields.GOLTITLE, TextFields.GOLSUM),
+            };
 
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "LandDreugh", "Wisp" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.MONTITLE2, TextFields.MONSUM2, new string[] { "LandDreugh", "Wisp" } });
-
-            // Undead pages
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "SkeletalSoldier", "GloomWraith", "FadedGhost", "Ghoul", "DireGhoul" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.UNDTITLE, TextFields.UNDSUM, new string[] { "SkeletalSoldier",
-            "GloomWraith", "FadedGhost", "Ghoul", "DireGhoul" } });
-
-            // Daedra pages
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "HellHound", "FireDaemon", "Dremora", "Scamp" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.DAETITLE, TextFields.DAESUM, new string[] { "HellHound", "FireDaemon", "Dremora", "Scamp" } });
-
-            // Golem pages
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", new object[] { "IronGolem", "IceGolem", "StoneGolem", "DwarvenSphere", "DwarvenSteam" });
-            ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { TextFields.GOLTITLE, TextFields.GOLSUM, new string[] { "IronGolem", "IceGolem", "StoneGolem", "DwarvenSphere", "DwarvenSteam" } });
+            foreach (var group in bestiaryGroups)
+            {
+                string[] entities = group.Entities;
+                string title = group.Title;
+                string summary = group.Summary;
+                ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_ENTITY", entities);
+                ModManager.Instance.SendModMessage("Bestiary", "REGISTER_CUSTOM_PAGE", new object[] { title, summary, entities });
+            }
 
             mod.IsReady = true;
         }
